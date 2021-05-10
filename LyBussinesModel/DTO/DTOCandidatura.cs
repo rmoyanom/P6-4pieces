f84badcb819d5ae88de433bdba02be27e98e28f8
Ricardo <72918409+rmoyanom@users.noreply.github.com>
2021-05-10 18:33:17 +0200

    dto y creacion

---
+++ LyBussinesModel/DTO/DTOCandidatura.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyBussinesModel.DTO
+{
+    public class DTOCandidatura
+    {
+        public int Id;
+        public Usuario Candidato;
+        public Servicio Servicio;
+        public EstadoCandidatura Estado;
+
+        public DateTime FechaInscripcion;
+        public int HorasRequeridas;
+
+        //Valores de finalización
+        public int Valoracion;
+        public String Comentarios;
+        public int HorasGanadas;
+
+        //Valores de Aceptación
+        public DateTime FechaAceptacion;
+        public DateTime FechaAcordadaServicio;
+        public int HorasAcordadas;
+
+        //Valores de Cancelacion
+        public DateTime FechaDeCancelacion;
+        public String MotivoCancelacíon;
+        public CanceladoPor AutorCancelado;
+
+        public DTOCandidatura()
+        {
+
+        }
+
+        public DTOCandidatura(int id)
+        {
+            Id = id;
+        }
+
+        public DTOCandidatura(int id, Usuario candidato, Servicio servicio, EstadoCandidatura estado, DateTime fechaInscripcion, int horasRequeridas, int valoracion, string comentarios, int horasGanadas, DateTime fechaAceptacion, DateTime fechaAcordadaServicio, int horasAcordadas, DateTime fechaDeCancelacion, string motivoCancelacíon, CanceladoPor autorCancelado) : this(id)
+        {
+            Candidato = candidato;
+            Servicio = servicio;
+            Estado = estado;
+            FechaInscripcion = fechaInscripcion;
+            HorasRequeridas = horasRequeridas;
+            Valoracion = valoracion;
+            Comentarios = comentarios;
+            HorasGanadas = horasGanadas;
+            FechaAceptacion = fechaAceptacion;
+            FechaAcordadaServicio = fechaAcordadaServicio;
+            HorasAcordadas = horasAcordadas;
+            FechaDeCancelacion = fechaDeCancelacion;
+            MotivoCancelacíon = motivoCancelacíon;
+            AutorCancelado = autorCancelado;
+        }
+    }
+}
--- LyDataAcces/DAO/DaoCandidatura.cs
+++ LyDataAcces/DAO/DaoCandidatura.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,63 @@ namespace LyDataAcces.DAO
             set => _Errores = value;
         }
         //Crear candidatura
+
+        public bool CrearCandicatura(LyBussinesModel.DTO.DTOCandidatura candicatura)
+        {
+
+            try
+            {
+                using (ORM.EFBancoTiempo db = new ORM.EFBancoTiempo())
+                {
+                    //Se realiza una busqueda del nombre en BD
+                    var query = from b in db.Usuarios
+                                where b.id == candicatura.Id
+                                select b;
+
+
+
+                    //Impide registrar un usuario que ya existe.
+                    Debug.Write(query.Count().ToString());
+
+
+                    if (query.Count() > 0)
+                    {
+                        throw new Exception("La candicatura ya existe");
+                    }
+
+        ORM.Candidatura newcandicatura = new ORM.Candidatura();
+                    //esta puesto como un string estado en la base de datos y no int
+                    newcandicatura.estado = candicatura.Estado;
+                    newcandicatura.fechaInscripcion = candicatura.FechaInscripcion;
+                    newcandicatura.idServicio = candicatura.Servicio.Id;
+                    newcandicatura.idUsuario = candicatura.Candidato.Id;
+                    newcandicatura.horasRequeridas = candicatura.HorasRequeridas;
+                    newcandicatura.fechaInscripcion = candicatura.FechaInscripcion;
+                    newcandicatura.Candidatura_Finalizada.valoracion = candicatura.Valoracion;
+                    newcandicatura.Candidatura_Finalizada.comentarios = candicatura.Comentarios;
+                    newcandicatura.Candidatura_Finalizada.horasGanadas = candicatura.HorasGanadas;
+                    newcandicatura.Candidatura_Aceptada.fechaAceptacion = candicatura.FechaAceptacion;
+                    newcandicatura.Candidatura_Aceptada.fechaAcordadaServicio = candicatura.FechaAcordadaServicio;
+                    //Esta puesto como un datetime en la base de datos y no int
+                    newcandicatura.Candidatura_Aceptada.horasAcordadas = candicatura.HorasAcordadas;
+                    newcandicatura.Candidatura_Cancelada.fechaCancelacion = candicatura.FechaDeCancelacion;
+                    newcandicatura.Candidatura_Cancelada.motivoCancelacion = candicatura.MotivoCancelacíon;
+                    //No esta en la base de datos pero si esta delcarados en candidatura
+                    newcandicatura.Candidatura_Cancelada.AutorCancelado = candicatura.MotivoCancelacíon;
+
+                    db.Candidatura.Add(newcandicatura);
+                    db.SaveChanges();
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _Errores = ex;
+                return false;
+            }
+
+        }
         //Cancelar candidatura
         //Aceptar candidatura
 

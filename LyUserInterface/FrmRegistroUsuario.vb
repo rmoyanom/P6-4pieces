7ea5ba1bad100e210e3924403fc4a8371275f9a4
Jaime Calafat <jcalafatb@uoc.edu>
2021-05-29 00:56:16 +0200

    Registro Usuario

--- LyUserInterface/FrmRegistroUsuario.vb
+++ LyUserInterface/FrmRegistroUsuario.vb
@@ -1,9 +1,35 @@
-﻿Public Class FrmRegistroUsuario
-    Private Sub FrmRegistroUsuario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports LyBussinesModel
+Imports LyDataAcces
+Public Class FrmRegistroUsuario
 
+    Private Sub BtnRegistrarUsuario_Click(sender As Object, e As EventArgs) Handles BtnRegistrar.Click
+
+        Dim nuevoUsuario = New DTO.DTOUsuario(
+            TxtNombreUsuario.Text,
+            TxtNombre.Text,
+            TxtApellidos.Text,
+            TxtTelefono.Text,
+            TxtEmail.Text,
+            Usuario.CreateHash(TxtNombreUsuario.Text, TxtPassword.Text)
+        )
+
+        Dim daoUsuario = New DAO.DaoUsuario()
+
+        If (daoUsuario.RegistrarUsuario(nuevoUsuario)) Then
+
+            Login(TxtNombreUsuario.Text, TxtPassword.Text)
+            FrmMain.Show()
+            Me.Hide()
+
+        Else
+            LblError.Text = daoUsuario.getError()
+            MsgBox("Error producido: " + LblError.Text, MsgBoxStyle.Critical, "Error registrando usuario")
+
+        End If
     End Sub
-
-    Private Sub FrmRegistroUsuario_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+    Private Sub BtnCancelarRegistro_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
         FrmInicioSesion.Show()
+        Me.Hide()
     End Sub
+
 End Class

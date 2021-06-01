ee587a0c4cab5e0d91049384c3b9068b66bbb4a6
Jaime Calafat <jcalafatb@uoc.edu>
2021-06-01 20:28:42 +0200

    mis servicios: botones cargan ventana

--- LyUserInterface/FrmMisServicios.vb
+++ LyUserInterface/FrmMisServicios.vb
@@ -1,4 +1,7 @@
-﻿Public Class FrmMisServicios
+﻿Imports LyBussinesModel
+Imports LyDataAcces
+
+Public Class FrmMisServicios
 
     Private Sub Volver_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
         Main.VolverAlMenu(Me)
@@ -7,4 +10,31 @@
     Private Sub FrmMisServicios_VisibleChanged(sender As Object, e As EventArgs) Handles Me.VisibleChanged
         Main.AbribVentana(Me)
     End Sub
+
+
+    Private Sub UcListaServicios_Load(sender As Object, e As EventArgs) Handles UcListaServicios.Load
+        Dim listaServiciosUser As List(Of DTO.DTOServicios)
+        LblCurrentMenu.Text = "Activos"
+        UploadListaServicios(listaServiciosUser, True)
+    End Sub
+
+    Private Sub UploadListaServicios(listaServiciosUser As List(Of DTO.DTOServicios), estado As Boolean)
+        Dim dao As DAO.DaoServicios = New DAO.DaoServicios()
+        Dim dto As DTO.DTOServicios = New DTO.DTOServicios()
+        listaServiciosUser = dao.GetListServicios(UsuarioAutenticado.Id, estado)
+    End Sub
+    Private Sub BtnActivos_Click(sender As Object, e As EventArgs) Handles BtnActivos.Click
+        Dim listaServiciosUser As List(Of DTO.DTOServicios)
+        LblCurrentMenu.Text = "Activos"
+        UploadListaServicios(listaServiciosUser, True)
+    End Sub
+    Private Sub BtnFinalizados_Click(sender As Object, e As EventArgs) Handles BtnFinalizados.Click
+        Dim listaServiciosUser As List(Of DTO.DTOServicios)
+        LblCurrentMenu.Text = "Finalizados"
+        UploadListaServicios(listaServiciosUser, False)
+    End Sub
+    Private Sub BtnVolver_Click(sender As Object, e As EventArgs) Handles BtnVolver.Click
+        Dispose()
+    End Sub
+
 End Class
